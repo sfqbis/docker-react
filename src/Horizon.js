139a0cc532893bba73f2156f99627d551812a9c1
sfqbis <sfqbis@gmail.com>
2021-01-02 16:52:41 +0100

    edit tests to pass

--- src/Horizon.js
+++ src/Horizon.js
@@ -4,12 +4,10 @@ function Horizon() {
   return (
     <div className="Horizon">
       <header></header>
-      <body>
-        <div>
-          <h1>Horizon Cube</h1>
-          <p>hello from event horizon</p>
-        </div>
-      </body>
+      <div>
+        <h1>Horizon 2 Cube</h1>
+        <p>hello from event horizon</p>
+      </div>
     </div>
   );
 }
